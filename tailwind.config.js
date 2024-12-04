c12fdff91c854832ad723be5abc2343f521c415a
Sam <schalder24@gmail.com>
2024-12-04 13:41:59 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // Enable dark mode
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
     extend: {},
