76871ddfa6224a11e906cd42221122a9bce6ede3
Sam <schalder24@gmail.com>
2024-12-06 04:43:31 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
   build: {
     outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
   },
 });
