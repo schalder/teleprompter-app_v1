32a933e6254f10d2bb04592c50623af8ec8dbf8a
Sam <schalder24@gmail.com>
2024-12-06 04:10:25 +0600

    Update CameraPreview.tsx

--- src/components/CameraPreview.tsx
+++ src/components/CameraPreview.tsx
@@ -1,1 +1,11 @@
+import React from "react";
 
+const CameraPreview: React.FC<{ previewVideo: string }> = ({ previewVideo }) => {
+  return (
+    <div className="border border-gray-700 p-4 rounded-md">
+      <video src={previewVideo} controls className="w-full h-auto" />
+    </div>
+  );
+};
+
+export default CameraPreview;
